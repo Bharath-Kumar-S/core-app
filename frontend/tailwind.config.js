de95cf7dee73838412f603b7926c9a915724c7af
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2024-01-01 17:23:46 +0200

    setup tailwind again

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/../*.{js.ts.tsx.jsx}"],
+  content: [
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
